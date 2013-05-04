995912ba04c5b57fbfa8ca693990e849de393c34
rrusu <rsln@ubuntu.(none)>
2013-05-04 17:21:08 -0400

    working in one session

--- test/qtcp_acceptance_tests.erl
+++ test/qtcp_acceptance_tests.erl
@@ -2,21 +2,22 @@
 -include_lib("eunit/include/eunit.hrl").
 
 one_connection_test() ->
-	%ok = application:start(qtcp),
 
 	{ok, Sock} = gen_tcp:connect("localhost", 9000, [list, {packet, 0}]),
 	
 	send(Sock, "in one"),
-	%gen_tcp:send(Sock, <<"in two\r\n">>),
-	%gen_tcp:send(Sock, <<"in three\r\n">>),
+	send(Sock, "in two"),
+	send(Sock, "in three"),
 
-	Result = send(Sock, "out"),
+	R1 = send(Sock, "out"),
+	R2 = send(Sock, "out"),
+	R3 = send(Sock, "out"),
 
     gen_tcp:close(Sock),
 	
-	%application:stop(qtcp),
-	
-	?assertEqual("\"one\"\n", Result).
+	?assertEqual("\"one\"\n", R1),
+	?assertEqual("\"two\"\n", R2),
+	?assertEqual("\"three\"\n", R3).
 
 
 send(Sock, Message) ->
